a62b5e37a55bf6d4f63d6023c65f42575bd55201
Amaayu <118004037+Amaayu@users.noreply.github.com>
2022-11-13 22:51:16 +0530

    Add files via upload

---
+++ table by aashu.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    
+    printf("enter number of given any multipletion table\n");
+    scanf("%d", &a);
+    
+    printf("table of %d\n " ,a);
+    printf( "%d\n", a*1);
+    printf("%d\n",a*2);
+    printf("%d\n",a*3);
+    printf("%d\n",a*4);
+    printf("%d\n",a*5);
+    printf("%d\n",a*6);
+    printf("%d\n",a*7);
+    printf("%d\n",a*8);
+    printf("%d\n",a*9);
+    printf("%d\n",a*10);
+    
+    return 0;
+}
